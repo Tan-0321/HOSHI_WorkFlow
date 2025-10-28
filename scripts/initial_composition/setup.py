019accfba6912fe8932bae6680d65917885084ef
Bingyang. TAN <tanby@nao.cas.cn>
2025-10-28 17:47:10 +0900

    feat(scripts): complete the pipeline of making input initial structure data files.

---
+++ scripts/initial_composition/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+
+setup(
+	name="FileNameConvention",
+	version="0.0.1",
+	description="FileNameConvention utility module from HOSHI_WorkFlow",
+	py_modules=["FileNameConvention"],
+	include_package_data=False,
+)
---
+++ scripts/make_initial_models/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+
+setup(
+	name="MakeInitialModels",
+	version="0.0.1",
+	description="MakeInitialModels utility module from HOSHI_WorkFlow",
+	py_modules=["MakeInitialModels"],
+	include_package_data=False,
+)
